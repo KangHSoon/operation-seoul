0dd6cb5b3366cf845f593e679a5964b9db0be6d8
KHS <gudtnslwkd@naver.com>
2026-04-18 23:59:40 +0900

    feat: Mission Entity-Repository-Service-Controller 레이어 구축 및 DB 연결 설정 완료

---
+++ backend/src/main/java/com/operation/seoul/OperationSeoulApplication.java
@@ -0,0 +1,11 @@
+package com.operation.seoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationSeoulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OperationSeoulApplication.class, args);
+    }
+}
